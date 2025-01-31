5069670428ec54afdf72170acca33ecda35aa5b5
mlmai105 <mollyle43@gmail.com>
2025-01-31 21:51:51 +0000

    yay

--- wordle.cpp
+++ wordle.cpp
@@ -12,13 +12,14 @@
 #include <vector>    // std::vector
 
 std::vector<int> createSecret(int size){
-    std::vector<int> code = {}
-    int count = 0 
-    while (count < 4) // repeat 4 times 
+    std::vector<int> code = {};
+    int counter = 0;
+
+    while (counter < size)
     {
         int val = rand() % 10;
         append(code, val);       //code.push_back(val);
-        counter = counter = 1; 
+        counter = counter + 1;
     } 
     
     return code; 
@@ -27,20 +28,27 @@ std::vector<int> createSecret(int size){
 
 std::vector<std::string> getHint(std::vector<int> secret, std::vector<int> guess){
     // Write this procedure here
-    std::vector<int> secret = {} 
-    std::vector<int> guess = {}
+   std::vector<std::string> hint = {};
     int curr_index = 0;
 
-    while(user_guess != code)
+    while(curr_index < length(guess))
     {
-        if (curr_index < LENGTH(guess))
+        if (guess[curr_index] != secret[curr_index])
         {
-            int curr_index != 
+            // append(hint, "X");
+            hint.push_back("X");
+
         }
-    }   count = count + 1;
+        else
+        {
+            // append(hint, "O");
+            hint.push_back("O");
+        }
+        curr_index = curr_index + 1;
+    }  
     
 
-    return {"X"};    // replace this with your code
+    return hint;
 }
 
 bool winGame(std::vector<int> secret, std::vector<int> guess)
@@ -56,14 +64,15 @@ int main()
     // Uncomment the code when you are finished testing your program
     // and want to have the program run for real
     // srand(time(0));
-    
-    std::vector<int> secret_code = createSecret(4);
-    std::cout << "Here is the secret code: " << int randum_num << std::endl;
-         
     srand(time(0)); 
+    std::vector<int> secret_code = createSecret(4);
+    std::cout << "Here is the secret code: ";
+    display(secret_code);
+         
+    
     int randum_num = rand() % 10; 
    
-    std::vector<int> user_guess = {};
+    std::vector<int> user_guess = {-1, -1, -1, -1};
     std::vector<std::string> hint = {};    // an empty list
 
     int secret_code_length = 4;
